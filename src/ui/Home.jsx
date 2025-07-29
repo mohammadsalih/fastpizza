280972a670ce132e9d344988a14fc5a97598ff25
mohammadsalih <mohammadsalih.main@gmail.com>
2025-07-30 01:52:50 +0300

    first commit

---
+++ src/ui/Home.jsx
@@ -0,0 +1,17 @@
+import Cart from "../features/cart/Cart";
+
+function Home() {
+  return (
+    <div>
+      <h1>
+        The best pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+
+      <Cart />
+    </div>
+  );
+}
+
+export default Home;
